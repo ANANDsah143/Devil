3ac9539b694fd7f605d240b97d3b27a145fd8c9e
ANANDsah143 <159440216+ANANDsah143@users.noreply.github.com>
2024-02-12 14:50:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://devilop:devilop123@devilserv
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1001979737404)
+LOGGER_ID = int(getenv("LOGGER_ID", -1001979737404))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 5977523092))
+OWNER_ID = int(getenv("OWNER_ID", 2142655466))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
